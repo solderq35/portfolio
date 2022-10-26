63ed4622f50e430547143fa5586dcbf8bf0ed543
solderq35 <solderq35@gmail.com>
2022-10-26 14:03:26 -0700

    small ml

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -45,7 +45,7 @@ export default function Navbar() {
                     </Disclosure.Button>
                   </div>
                   <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="flex-shrink-0 flex items-center md:ml-5">
+                    <div className="flex-shrink-0 flex items-center sm:ml-5">
                       <img className="block lg:hidden h-8 w-auto" src="/favicon.ico" alt="Workflow" />
                       <img className="hidden lg:block h-8 w-auto" src="/favicon.ico" alt="Workflow" />
                     </div>
