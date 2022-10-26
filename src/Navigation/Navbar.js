6410ca50e73c60ad2b97b9abcfc5e2db6e4cc85f
solderq35 <solderq35@gmail.com>
2022-10-26 13:44:23 -0700

    hopefully fix icon center mobile

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -45,9 +45,9 @@ export default function Navbar() {
                     </Disclosure.Button>
                   </div>
                   <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="flex-shrink-0 flex items-center">
-                      <img className="block lg:hidden h-8 w-auto ml-5" src="/favicon.ico" alt="Workflow" />
-                      <img className="hidden lg:block h-8 w-auto ml-5" src="/favicon.ico" alt="Workflow" />
+                    <div className="flex-shrink-0 flex items-center sm: ml-0 md:ml-5 lg:ml-5 lg:ml-5 xl: ml-5 2xl: ml5">
+                      <img className="block lg:hidden h-8 w-auto" src="/favicon.ico" alt="Workflow" />
+                      <img className="hidden lg:block h-8 w-auto" src="/favicon.ico" alt="Workflow" />
                     </div>
                     <div className="hidden sm:block sm:ml-6">
                       <div className="flex space-x-4">
