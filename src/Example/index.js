5be87cc1eddf5cd9deefd6fcb712a7b5ac930e0f
solderq35 <solderq35@gmail.com>
2022-10-26 13:28:48 -0700

    fix navbar icon and navbar scroll

--- src/Example/index.js
+++ src/Example/index.js
@@ -3,30 +3,32 @@ var resumeLink = globalVal.value;
 
 export default function index() {
   return (
-    document.title = "Jeff Huang",
-    <div className="mx-auto w-full max-w-6xl pb-10 lg:py-12 lg:px-8 text-center">
-      <h1 className="text-8xl mt-20">
-        {' '}
-        <b>Jeff Huang </b>
-      </h1>
+    (document.title = 'Jeff Huang'),
+    (
+      <div className="mx-auto w-full max-w-6xl pb-10 lg:py-12 lg:px-8 text-center">
+        <h1 className="text-8xl mt-20">
+          {' '}
+          <b>Jeff Huang </b>
+        </h1>
 
-      <p className="text-2xl pb-5 py-10">
-        Hi, I'm a third year student at Oregon State University, pursuing a computer science degree with a focus on web
-        development.
-      </p>
+        <p className="text-2xl pb-5 py-10">
+          Hi, I'm a third year student at Oregon State University, pursuing a computer science degree with a focus on
+          web development.
+        </p>
 
-      <button class="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2.5 px-4 rounded my-6 text-2xl mx-5">
-        <a href="/projects" class="a2">
-          Projects
-        </a>
-      </button>
+        <button class="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2.5 px-4 rounded my-6 text-2xl mx-5">
+          <a href="/projects" class="a2">
+            Projects
+          </a>
+        </button>
 
-      <button class="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2.5 px-4 rounded my-6 text-2xl mx-5">
-        <a href={resumeLink} target="_blank" rel="noreferrer" class="a2">
-          Resume
-        </a>
-      </button>
-    </div>
+        <button class="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2.5 px-4 rounded my-6 text-2xl mx-5">
+          <a href={resumeLink} target="_blank" rel="noreferrer" class="a2">
+            Resume
+          </a>
+        </button>
+      </div>
+    )
   );
 }
 //little comment test
--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -26,71 +26,79 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        target={item.target}
-                        rel={item.rel}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'text-gray-100 hover:bg-gray-700 hover:text-white'
-                            : 'text-gray-100 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium text-lg'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+    <Fragment>
+      <header class="sticky top-0 z-50">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="">
+                <div className="relative flex items-center justify-between h-16">
+                  <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    {/* Mobile menu button*/}
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                  <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                    <div className="flex-shrink-0 flex items-center">
+                      <img className="block lg:hidden h-8 w-auto ml-5" src="/favicon.ico" alt="Workflow" />
+                      <img className="hidden lg:block h-8 w-auto ml-5" src="/favicon.ico" alt="Workflow" />
+                    </div>
+                    <div className="hidden sm:block sm:ml-6">
+                      <div className="flex space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            target={item.target}
+                            rel={item.rel}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'text-gray-100 hover:bg-gray-700 hover:text-white'
+                                : 'text-gray-100 hover:bg-gray-700 hover:text-white',
+                              'px-3 py-2 rounded-md text-sm font-medium text-lg'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    {/* Profile dropdown */}
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-              </div>
-            </div>
-          </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+              <Disclosure.Panel className="sm:hidden">
+                <div className="px-2 pt-2 pb-3 space-y-1">
+                  {navigation.map((item) => (
+                    <Disclosure.Button
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block px-3 py-2 rounded-md text-base font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  ))}
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </header>
+    </Fragment>
   );
 }
