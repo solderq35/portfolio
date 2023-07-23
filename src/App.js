e6e492f4c8c1b8f945bcdf0214e119b60416b79f
solderq35 <solderq35@gmail.com>
2023-07-23 16:33:45 -0700

    404 fix for hashrouter maybe

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import Index from './Pages/index';
 import Projects from './Pages/projects';
 import Contact from './Pages/contact';
 import NotFound from './Pages/notFound';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" component={App} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
