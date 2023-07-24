e46e19b8f6e87cc0f715092fa6e2cdcaae566836
solderq35 <solderq35@gmail.com>
2023-07-23 17:04:33 -0700

    should fix hash routing 404 fr

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ import Index from './Pages/index';
 import Projects from './Pages/projects';
 import Contact from './Pages/contact';
 import NotFound from './Pages/notFound';
-import { HashRouter as Router, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" component={App} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Index />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
