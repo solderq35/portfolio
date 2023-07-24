7c3a2aac7f70d0ccd9a744b17c3b759a68d65932
solderq35 <solderq35@gmail.com>
2023-07-23 17:16:17 -0700

    give up on hash router 404 and revert commits

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Index from './Pages/index';
 import Projects from './Pages/projects';
 import Contact from './Pages/contact';
 import NotFound from './Pages/notFound';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Index />} />
-        <Route exact path="/projects" element={<Projects />} />
-        <Route exact path="/contact" element={<Contact />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
